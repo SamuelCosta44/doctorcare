67fa8685948ae32ab066c8722edafe3ec7602533
SamuelCosta44 <105761539+SamuelCosta44@users.noreply.github.com>
2022-05-17 23:02:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+    //activateMenuAtCurrentSection(services)
+  };   
+
+function activateMenuAtCurrentSection(section) {
+const targetLine = scrollY + innerHeight / 2
+
+
+
+
+
+//verificar se a seção passou da linha
+// quais dados vou precisar?
+
+//topo da seção
+const sectionTop = section.offsetTop
+
+//altura da seção
+const sectionHeight = section.offsetHeight
+
+
+//o topo da seção chegou a ultrapassar a linha alvo
+const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+
+//informações dos dados e da lógica
+console.log(
+  'O topo da seção chegou ou passou da linha?',
+  sectionTopReachOrPassedTargetLine
+  )
+
+
+//Verificar se a base está abaixo da linha alvo
+//quais dados vou precisar?
+
+//a seção termina onde?
+const sectionEndsAt = sectionTop + sectionHeight
+
+
+//o final da seção passou da linha alvo
+const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+console.log("O fundo da seção passou da linha?" , sectionEndPassedTargetLine)
+
+//limites da seção
+const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+const sectionId = section.getAttribute('id')
+const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+
+menuElement.classList.remove('active')
+if (sectionBoundaries) {
+  menuElement.classList.add('active')
+}
+
+}
+
+function showNavOnScroll(){
+if (scrollY > 0) {
+  navigation.classList.add('scroll')
+  } else {
+  navigation.classList.remove('scroll')
+  }
+}
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+};  
+ 
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+};
+
+ScrollReveal({
+  origin:'top',
+  distance:'30px',
+  duration: '700',
+}).reveal(`#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .cards
+#about,
+#about header,
+#about .content`)
+
+
+
